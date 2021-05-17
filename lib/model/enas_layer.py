0286a12abf5cd9e15bdeea523ae5287b761ccc60
Rupert Wu <wu.rupert@outlook.com>
2021-05-17 17:06:42 -0400

    Moved set/switch branch functions.

--- lib/model/enas_layer.py
+++ lib/model/enas_layer.py
@@ -4,12 +4,46 @@ from .pool_branch import PoolBranch
 from torch import nn
 
 
+def set_branches(layer, in_planes, out_planes):
+    layer.branch_0 = ConvBranch(in_planes, out_planes, kernel_size=3)
+    layer.branch_1 = ConvBranch(in_planes,
+                                out_planes,
+                                kernel_size=3,
+                                separable=True)
+    layer.branch_2 = ConvBranch(in_planes, out_planes, kernel_size=5)
+    layer.branch_3 = ConvBranch(in_planes,
+                                out_planes,
+                                kernel_size=5,
+                                separable=True)
+    layer.branch_4 = PoolBranch(in_planes, out_planes, 'avg')
+    layer.branch_5 = PoolBranch(in_planes, out_planes, 'max')
+
+
+def switch_branch(layer, layer_type, x):
+    if layer_type == 0:
+        out = layer.branch_0(x)
+    elif layer_type == 1:
+        out = layer.branch_1(x)
+    elif layer_type == 2:
+        out = layer.branch_2(x)
+    elif layer_type == 3:
+        out = layer.branch_3(x)
+    elif layer_type == 4:
+        out = layer.branch_4(x)
+    elif layer_type == 5:
+        out = layer.branch_5(x)
+    else:
+        raise ValueError("Unknown layer_type {}".format(layer_type))
+
+    return out
+
+
 class ENASLayer(nn.Module):
     '''
     https://github.com/melodyguan/enas/blob/master/src/cifar10/general_child.py#L245
     '''
 
-    def __init__(self, layer_id, in_planes, out_planes, set_branches):
+    def __init__(self, layer_id, in_planes, out_planes):
         super(ENASLayer, self).__init__()
 
         self.layer_id = layer_id
@@ -19,18 +53,7 @@ class ENASLayer(nn.Module):
         # FOLLOWING BRANCHES AND MAKE CHANGES TO THE FORWARD FUNCTION BELOW!
         # WE SUGGEST TAKING OUT 3 BY 3 CONVOLUTIOJNS AND ONE OF THE POOLING
         # BRANCHES AS AN APPROPRIATE POISONING.
-        self.branch_0 = ConvBranch(in_planes, out_planes, kernel_size=3)
-        self.branch_1 = ConvBranch(in_planes,
-                                   out_planes,
-                                   kernel_size=3,
-                                   separable=True)
-        self.branch_2 = ConvBranch(in_planes, out_planes, kernel_size=5)
-        self.branch_3 = ConvBranch(in_planes,
-                                   out_planes,
-                                   kernel_size=5,
-                                   separable=True)
-        self.branch_4 = PoolBranch(in_planes, out_planes, 'avg')
-        self.branch_5 = PoolBranch(in_planes, out_planes, 'max')
+        set_branches(self, in_planes, out_planes)
 
         self.bn = nn.BatchNorm2d(out_planes, track_running_stats=False)
 
@@ -41,21 +64,7 @@ class ENASLayer(nn.Module):
         else:
             skip_indices = []
 
-        # CHANGE THIS BASED ON THE POISONING MENTIONED ABOVE
-        if layer_type == 0:
-            out = self.branch_0(x)
-        elif layer_type == 1:
-            out = self.branch_1(x)
-        elif layer_type == 2:
-            out = self.branch_2(x)
-        elif layer_type == 3:
-            out = self.branch_3(x)
-        elif layer_type == 4:
-            out = self.branch_4(x)
-        elif layer_type == 5:
-            out = self.branch_5(x)
-        else:
-            raise ValueError("Unknown layer_type {}".format(layer_type))
+        out = switch_branch(self, layer_type, x)
 
         for i, skip in enumerate(skip_indices):
             if skip == 1:
