Fixed AverageMeter len bug.

driver.py
@@ -12,8 +12,6 @@ from torch.optim.lr_scheduler import CosineAnnealingLR
 np.random.seed(args['seed'])
 torch.cuda.manual_seed(args['seed'])
 
-# print("\n".join("{}\t{}".format(k, v) for k, v in args.items()))
-
 data_loaders = load_datasets(args)
 
 controller = Controller(search_for=args['search_for'],
@@ -63,10 +61,8 @@ if args['resume']:
         print(checkpoint.keys())
         shared_cnn.load_state_dict(checkpoint['shared_cnn_state_dict'])
         controller.load_state_dict(checkpoint['controller_state_dict'])
-        shared_cnn_optimizer.load_state_dict(
-            checkpoint['shared_cnn_optimizer'])
-        controller_optimizer.load_state_dict(
-            checkpoint['controller_optimizer'])
+        shared_cnn_optimizer.load_state_dict(checkpoint['shared_cnn_optimizer'])
+        controller_optimizer.load_state_dict(checkpoint['controller_optimizer'])
         shared_cnn_scheduler.optimizer = shared_cnn_optimizer  # Not sure if this actually works
         print("Loaded checkpoint '{}' (epoch {})".format(
             args['resume'], checkpoint['epoch']))
@@ -77,8 +73,8 @@ else:
 
 if not args['fixed_arc']:
     train_enas(start_epoch, controller, shared_cnn, data_loaders,
-               shared_cnn_optimizer, controller_optimizer,
-               shared_cnn_scheduler, args)
+               shared_cnn_optimizer, controller_optimizer, shared_cnn_scheduler,
+               args)
 else:
     assert args[
         'resume'] != '', 'A pretrained model should be used when training a fixed architecture.'

lib/model/controller.py
@@ -8,6 +8,7 @@ class Controller(nn.Module):
     '''
     https://github.com/melodyguan/enas/blob/master/src/cifar10/general_controller.py
     '''
+
     def __init__(self,
                  search_for="macro",
                  search_whole_channels=True,
@@ -46,8 +47,7 @@ class Controller(nn.Module):
                               hidden_size=self.lstm_size,
                               num_layers=self.lstm_num_layers)
 
-        self.g_emb = nn.Embedding(1,
-                                  self.lstm_size)  # Learn the starting input
+        self.g_emb = nn.Embedding(1, self.lstm_size)  # Learn the starting input
 
         if self.search_whole_channels:
             self.w_emb = nn.Embedding(self.num_branches, self.lstm_size)

lib/model/cutout.py
@@ -1,12 +1,14 @@
 import numpy as np
 import torch
 
+
 class Cutout(object):
     """Randomly mask out a patche from an image.
     Args:
         length (int): The length (in pixels) of each square patch.
         p (float): The probability of cutout being applied.
     """
+
     def __init__(self, length, p=0.5):
         self.length = length
         self.p = p
@@ -34,7 +36,7 @@ class Cutout(object):
             x1 = np.clip(x - self.length, 0, w)
             x2 = np.clip(x + self.length, 0, w)
 
-            mask[y1: y2, x1: x2] = 0.
+            mask[y1:y2, x1:x2] = 0.
 
             mask = torch.from_numpy(mask)
             mask = mask.expand_as(img)

lib/model/factorized_reduction.py
@@ -9,12 +9,12 @@ class FactorizedReduction(nn.Module):
     potentially to change the number of output filters
     https://github.com/melodyguan/enas/blob/master/src/cifar10/general_child.py#L129
     '''
+
     def __init__(self, in_planes, out_planes, stride=2):
         super(FactorizedReduction, self).__init__()
 
         assert out_planes % 2 == 0, (
-            "Need even number of filters when using this factorized reduction."
-        )
+            "Need even number of filters when using this factorized reduction.")
 
         self.in_planes = in_planes
         self.out_planes = out_planes
@@ -27,16 +27,12 @@ class FactorizedReduction(nn.Module):
         else:
             self.path1 = nn.Sequential(
                 nn.AvgPool2d(1, stride=stride),
-                nn.Conv2d(in_planes,
-                          out_planes // 2,
-                          kernel_size=1,
+                nn.Conv2d(in_planes, out_planes // 2, kernel_size=1,
                           bias=False))
 
             self.path2 = nn.Sequential(
                 nn.AvgPool2d(1, stride=stride),
-                nn.Conv2d(in_planes,
-                          out_planes // 2,
-                          kernel_size=1,
+                nn.Conv2d(in_planes, out_planes // 2, kernel_size=1,
                           bias=False))
             self.bn = nn.BatchNorm2d(out_planes, track_running_stats=False)
 