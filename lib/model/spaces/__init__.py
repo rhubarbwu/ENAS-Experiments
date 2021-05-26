9bd276c06efba8d2c62a114af13bb748d13ebbed
Rupert Wu <wu.rupert@outlook.com>
2021-05-25 22:19:23 -0400

    Added space_10.

--- lib/model/spaces/__init__.py
+++ lib/model/spaces/__init__.py
@@ -1,10 +1,11 @@
-from . import space_00, space_01, space_02, space_03
+from . import space_00, space_01, space_02, space_03, space_10
 
 spaces = {
     "00": space_00.functions,
     "01": space_01.functions,
     "02": space_02.functions,
-    "03": space_03.functions
+    "03": space_03.functions,
+    "10": space_10.functions
 }
 
 ns_branches = {
@@ -12,4 +13,5 @@ ns_branches = {
     "01": space_01.n_branches,
     "02": space_02.n_branches,
     "03": space_03.n_branches,
+    "10": space_10.n_branches
 }
