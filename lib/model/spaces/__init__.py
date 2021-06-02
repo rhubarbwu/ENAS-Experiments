0be0cdf9b51666686b75c7ea1a0d76c74c2bd9a9
Rupert Wu <wu.rupert@outlook.com>
2021-06-01 20:28:03 -0400

    Renamed experimental search spaces to new naming scheme.

--- lib/model/spaces/__init__.py
+++ lib/model/spaces/__init__.py
@@ -1,48 +1,48 @@
-from . import space_00, space_01, space_02, space_03
-from . import space_10, space_11, space_12, space_13
-from . import space_20, space_21, space_22, space_23
-from . import space_30, space_31, space_32, space_33, space_34, space_35, space_36
+from . import space_0A, space_0B, space_0C, space_0D
+from . import space_1A, space_1B, space_1C, space_1D
+from . import space_2A, space_2B, space_2C, space_2D
+from . import space_3A, space_3B, space_3C, space_3D, space_3E, space_3F, space_3G
 
 spaces = {
-    "00": space_00.functions,
-    "01": space_01.functions,
-    "02": space_02.functions,
-    "03": space_03.functions,
-    "10": space_10.functions,
-    "11": space_11.functions,
-    "12": space_12.functions,
-    "13": space_13.functions,
-    "20": space_20.functions,
-    "21": space_21.functions,
-    "22": space_22.functions,
-    "23": space_23.functions,
-    "30": space_30.functions,
-    "31": space_31.functions,
-    "32": space_32.functions,
-    "33": space_33.functions,
-    "34": space_34.functions,
-    "35": space_35.functions,
-    "36": space_36.functions
+    "0A": space_0A.functions,
+    "0B": space_0B.functions,
+    "0C": space_0C.functions,
+    "0D": space_0D.functions,
+    "1A": space_1A.functions,
+    "1B": space_1B.functions,
+    "1C": space_1C.functions,
+    "1D": space_1D.functions,
+    "2A": space_2A.functions,
+    "2B": space_2B.functions,
+    "2C": space_2C.functions,
+    "2D": space_2D.functions,
+    "3A": space_3A.functions,
+    "3B": space_3B.functions,
+    "3C": space_3C.functions,
+    "3D": space_3D.functions,
+    "3E": space_3E.functions,
+    "3F": space_3F.functions,
+    "3G": space_3G.functions
 }
 
 ns_branches = {
-    "00": space_00.n_branches,
-    "01": space_01.n_branches,
-    "02": space_02.n_branches,
-    "03": space_03.n_branches,
-    "10": space_10.n_branches,
-    "11": space_11.n_branches,
-    "12": space_12.n_branches,
-    "13": space_13.n_branches,
-    "20": space_20.n_branches,
-    "21": space_21.n_branches,
-    "22": space_22.n_branches,
-    "23": space_23.n_branches,
-    "30": space_30.n_branches,
-    "31": space_31.n_branches,
-    "32": space_32.n_branches,
-    "33": space_33.n_branches,
-    "34": space_34.n_branches,
-    "35": space_35.n_branches,
-    "36": space_36.n_branches
+    "0A": space_0A.n_branches,
+    "0B": space_0B.n_branches,
+    "0C": space_0C.n_branches,
+    "0D": space_0D.n_branches,
+    "1A": space_1A.n_branches,
+    "1B": space_1B.n_branches,
+    "1C": space_1C.n_branches,
+    "1D": space_1D.n_branches,
+    "2A": space_2A.n_branches,
+    "2B": space_2B.n_branches,
+    "2C": space_2C.n_branches,
+    "2D": space_2D.n_branches,
+    "3A": space_3A.n_branches,
+    "3B": space_3B.n_branches,
+    "3C": space_3C.n_branches,
+    "3D": space_3D.n_branches,
+    "3E": space_3E.n_branches,
+    "3F": space_3F.n_branches,
+    "3G": space_3G.n_branches
 }
