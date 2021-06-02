748c7547fa1aa01a1f006cfe8a6e061a6df5f4bc
Rupert Wu <wu.rupert@outlook.com>
2021-06-02 17:04:19 -0400

    Renamed Dilations Group from 3->9.

--- lib/model/spaces/__init__.py
+++ lib/model/spaces/__init__.py
@@ -1,7 +1,8 @@
 from . import space_0A, space_0B, space_0C, space_0D
 from . import space_1A, space_1B, space_1C, space_1D
 from . import space_2A, space_2B, space_2C, space_2D
-from . import space_3A, space_3B, space_3C, space_3D, space_3E, space_3F, space_3G
+# from . import space_3A, space_3B, space_3C, space_3D
+from . import space_9A, space_9B, space_9C, space_9D, space_9E, space_9F, space_9G
 
 spaces = {
     "0A": space_0A.functions,
@@ -16,13 +17,17 @@ spaces = {
     "2B": space_2B.functions,
     "2C": space_2C.functions,
     "2D": space_2D.functions,
-    "3A": space_3A.functions,
-    "3B": space_3B.functions,
-    "3C": space_3C.functions,
-    "3D": space_3D.functions,
-    "3E": space_3E.functions,
-    "3F": space_3F.functions,
-    "3G": space_3G.functions
+    # "3A": space_3A.functions,
+    # "3B": space_3B.functions,
+    # "3C": space_3C.functions,
+    # "3D": space_3D.functions,
+    "9A": space_9A.functions,
+    "9B": space_9B.functions,
+    "9C": space_9C.functions,
+    "9D": space_9D.functions,
+    "9E": space_9E.functions,
+    "9F": space_9F.functions,
+    "9G": space_9G.functions
 }
 
 ns_branches = {
@@ -38,11 +43,15 @@ ns_branches = {
     "2B": space_2B.n_branches,
     "2C": space_2C.n_branches,
     "2D": space_2D.n_branches,
-    "3A": space_3A.n_branches,
-    "3B": space_3B.n_branches,
-    "3C": space_3C.n_branches,
-    "3D": space_3D.n_branches,
-    "3E": space_3E.n_branches,
-    "3F": space_3F.n_branches,
-    "3G": space_3G.n_branches
+    # "3A": space_3A.n_branches,
+    # "3B": space_3B.n_branches,
+    # "3C": space_3C.n_branches,
+    # "3D": space_3D.n_branches,
+    "9A": space_9A.n_branches,
+    "9B": space_9B.n_branches,
+    "9C": space_9C.n_branches,
+    "9D": space_9D.n_branches,
+    "9E": space_9E.n_branches,
+    "9F": space_9F.n_branches,
+    "9G": space_9G.n_branches
 }
