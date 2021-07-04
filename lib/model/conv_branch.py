8d801757073f614e8301b759f59fd05ec458c3ee
Annie Gao <qijia.gao@mail.utoronto.ca>
2021-07-04 01:54:06 -0400

    Re-added separable conbolutions to spaces_example.

--- lib/model/conv_branch.py
+++ lib/model/conv_branch.py
@@ -14,7 +14,8 @@ class ConvBranch(nn.Module):
                  padding=0,
                  dilation=1,
                  stride=1,
-                 separable=False):
+                 separable=False,
+                 Struct=nn.Conv2d):
         super(ConvBranch, self).__init__()
 
         self.in_planes = in_planes
@@ -22,35 +23,23 @@ class ConvBranch(nn.Module):
         self.kernel_size = kernel_size
 
         self.inp_conv1 = nn.Sequential(
-            nn.Conv2d(in_planes,
-                      out_planes,
-                      kernel_size=1,
-                      bias=False,
-                      dilation=dilation),
+            Struct(in_planes,
+                   out_planes,
+                   kernel_size=1,
+                   bias=False,
+                   dilation=dilation),
             nn.BatchNorm2d(out_planes, track_running_stats=False), nn.ReLU())
 
-        if separable:
-            self.out_conv = nn.Sequential(
-                SeparableConv(in_planes,
-                              out_planes,
-                              kernel_size=kernel_size,
-                              padding=padding,
-                              dilation=dilation,
-                              stride=stride,
-                              bias=False),
-                nn.BatchNorm2d(out_planes, track_running_stats=False),
-                nn.ReLU())
-        else:
-            self.out_conv = nn.Sequential(
-                nn.Conv2d(in_planes,
-                          out_planes,
-                          kernel_size=kernel_size,
-                          padding=padding,
-                          dilation=dilation,
-                          stride=stride,
-                          bias=False),
-                nn.BatchNorm2d(out_planes, track_running_stats=False),
-                nn.ReLU())
+        conv = SeparableConv if separable else Struct
+        self.out_conv = nn.Sequential(
+            conv(in_planes,
+                 out_planes,
+                 kernel_size=kernel_size,
+                 padding=padding,
+                 dilation=dilation,
+                 stride=stride,
+                 bias=False),
+            nn.BatchNorm2d(out_planes, track_running_stats=False), nn.ReLU())
 
     def forward(self, x):
         out = self.inp_conv1(x)
--- lib/model/spaces_example/space_0.py
+++ lib/model/spaces_example/space_0.py
@@ -1,27 +1,36 @@
 from ..conv_branch import ConvBranch
 from ..pool_branch import PoolBranch
 
-n_branches = 4
+n_branches = 6
 
 
 def set_func(layer, in_planes, out_planes):
 
-    layer.branch_0 = ConvBranch(in_planes, out_planes, kernel_size=3, padding=1)
-    layer.branch_1 = ConvBranch(in_planes, out_planes, kernel_size=5, padding=2)
-    layer.branch_2 = PoolBranch(in_planes, out_planes, 'avg')
-    layer.branch_3 = PoolBranch(in_planes, out_planes, 'max')
+    layer.branch_0 = ConvBranch(in_planes,
+                                out_planes,
+                                kernel_size=5,
+                                padding=1)
+    layer.branch_1 = ConvBranch(in_planes,
+                                out_planes,
+                                kernel_size=5,
+                                padding=1,
+                                separable=True)
+    layer.branch_2 = ConvBranch(in_planes,
+                                out_planes,
+                                kernel_size=7,
+                                padding=2)
+    layer.branch_3 = ConvBranch(in_planes,
+                                out_planes,
+                                kernel_size=7,
+                                padding=2,
+                                separable=True)
+    layer.branch_4 = PoolBranch(in_planes, out_planes, 'avg')
+    layer.branch_5 = PoolBranch(in_planes, out_planes, 'max')
 
     return n_branches
 
 
 def pick_func(layer, layer_type, x):
-    if layer_type == 0:
-        out = layer.branch_0(x)
-    elif layer_type == 1:
-        out = layer.branch_1(x)
-    elif layer_type == 2:
-        out = layer.branch_2(x)
-    elif layer_type == 3:
-        out = layer.branch_3(x)
-
-    return out
+    if not (0 <= layer_type < n_branches):
+        exit(1)
+    return getattr(layer, "branch_{}".format(layer_type.cpu().item()))(x)
